e07d0e43983dfb9a631d48d0744ffaaa839d9077
Julita Bielik <julz@o2.pl>
2014-05-07 15:18:58 +0200

    update

--- src/com/example/checkmate/Main.java
+++ src/com/example/checkmate/Main.java
@@ -11,7 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 
-public class MainActivity extends Activity {
+public class Main extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
